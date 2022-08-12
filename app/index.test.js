deceb2aa32c81d29724eef3bb6365d5bb6c1afe4
CharlesAD <c.acquahdavis@gamil.com>
2022-08-12 13:50:13 +0100

    Broken tests

---
+++ app/index.test.js
@@ -0,0 +1,11 @@
+import { tryConvert } from "./index.js";
+
+describe("testing temperature functions", () => {
+  test("testing to see if function correctly converts temperatures", () => {
+    expect(tryConvert(100, fahrenheit)).toBe(38);
+  });
+
+  test("testing to see if function correctly converts temperatures", () => {
+    expect(tryConvert(100, celsius)).toBe(212);
+  });
+});
