869b91de0f574455846ceaaa0a4837eab9860a42
CharlesAD <c.acquahdavis@gamil.com>
2022-08-12 14:51:11 +0100

    correct first 2 scenarios

--- app/index.test.js
+++ app/index.test.js
@@ -1,11 +1,11 @@
-import { tryConvert, celsius, fahrenheit } from "./index.js";
+import tryConvert from "./index.js";
 
 describe("testing temperature functions", () => {
   test("testing to see if function correctly converts temperatures", () => {
-    expect(tryConvert(100, fahrenheit)).toBe(38);
+    expect(tryConvert(100, "toFahrenheit")).toBe("212");
   });
+});
 
-  test("testing to see if function correctly converts temperatures", () => {
-    expect(tryConvert(100, celsius)).toBe(212);
-  });
+test("testing to see if function correctly converts temperatures", () => {
+  expect(tryConvert(100, "toCelsius")).toBe("37.778");
 });
