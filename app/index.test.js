0e9d2b669b254702904580c22df80b9645832d80
CharlesAD <c.acquahdavis@gamil.com>
2022-08-15 10:50:11 +0100

    complete all tests

--- app/index.test.js
+++ app/index.test.js
@@ -1,11 +1,19 @@
 import tryConvert from "./index.js";
 
 describe("testing temperature functions", () => {
-  test("testing to see if function correctly converts temperatures", () => {
+  test("testing to see if function correctly converts temperatures from celsius to fahrenheit", () => {
     expect(tryConvert(100, "toFahrenheit")).toBe("212");
   });
 });
 
-test("testing to see if function correctly converts temperatures", () => {
-  expect(tryConvert(100, "toCelsius")).toBe("37.778");
+test("testing to see if function correctly converts temperatures from fahrenheit to celsius", () => {
+  expect(tryConvert(50, "toCelsius")).toBe("10");
+});
+
+test("testing to see if the function correctly converts non-numerical inputs to Celsius", () => {
+  expect(tryConvert("abc", "toCelsius")).toBe("");
+});
+
+test("testing to see if the function correctly converts non-numerical inputs to Fahrenheit", () => {
+  expect(tryConvert("abc", "toFahrenheit")).toBe("");
 });
